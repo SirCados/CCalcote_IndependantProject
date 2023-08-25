8c82e096bbabeb6b44197555057adf17b45acca0
Cade Calcote <cade.m.calcote@gmail.com>
2023-08-25 15:20:20 -0700

    Movement
    Movement works for keyboard and wired nintendo switch gamecube style controller.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,15 @@ public class PlayerController : MonoBehaviour
 
     PlayerInput _playerInput;
     Rigidbody _playerRigidBody;
+    InputAction _moveAction;
+    InputAction _jumpAction;
 
     private void Awake()
     {
         _playerInput = GetComponent<PlayerInput>();
         _playerRigidBody = GetComponent<Rigidbody>();
+        _moveAction = _playerInput.actions["Move"];
+        _jumpAction = _playerInput.actions["Jump"];
     }
 
     // Start is called before the first frame update
@@ -22,11 +26,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
     }
 
     void MovePlayer()
     {
-        
+        Vector2 inputVector = _moveAction.ReadValue<Vector2>();
+        Vector3 movementVector = new Vector3(inputVector.x, 0, inputVector.y);
+
+        _playerRigidBody.AddForce(movementVector.normalized * 10f, ForceMode.Force);
     }
 }
