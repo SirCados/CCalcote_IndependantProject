1fc61cad32a1d38b158dbb7b4007daa77ec33674
Cade Calcote <cade.m.calcote@gmail.com>
2023-08-25 15:58:02 -0700

    Jumping and Air Dashing
    Basic jumping and air dashing. Need to find a way to reset velocity to 0 when air dashing to explore different aerial handlings. Need to explore more ground handling.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,10 @@ public class PlayerController : MonoBehaviour
     Rigidbody _playerRigidBody;
     InputAction _moveAction;
     InputAction _jumpAction;
+    string _airActionMap = "Air";
+    string _groundActionMap = "Ground";
+    public bool _isGrounded = true;
+    
 
     private void Awake()
     {
@@ -29,11 +33,52 @@ public class PlayerController : MonoBehaviour
         MovePlayer();
     }
 
+    private void FixedUpdate()
+    {
+        MovePlayer();
+        JumpPlayer();
+        GroundCheck();
+    }
+
     void MovePlayer()
     {
         Vector2 inputVector = _moveAction.ReadValue<Vector2>();
         Vector3 movementVector = new Vector3(inputVector.x, 0, inputVector.y);
+        if (_playerInput.currentActionMap.name == _groundActionMap)
+        {
+            _playerRigidBody.AddForce(movementVector.normalized * 5f, ForceMode.Force);
+        }
+        else if (_playerInput.currentActionMap.name == _airActionMap)
+        {
+            _playerRigidBody.AddForce(movementVector.normalized * 1f, ForceMode.Force);
+        }
+        
+    }
 
-        _playerRigidBody.AddForce(movementVector.normalized * 10f, ForceMode.Force);
+    void JumpPlayer()
+    {
+        Vector2 inputVector = _moveAction.ReadValue<Vector2>();
+        if (_jumpAction.IsPressed() && _isGrounded)
+        {
+            Vector3 movementVector = new Vector3(0, 1, 0);
+            _playerRigidBody.AddForce(movementVector.normalized * 2f, ForceMode.Impulse);
+            print("jump");
+            _isGrounded = false;
+        } 
+        else if (_jumpAction.IsPressed() && !_isGrounded)
+        {
+            Vector3 movementVector = new Vector3(inputVector.x, 0, inputVector.y);
+            _playerRigidBody.AddForce(movementVector.normalized * 5f, ForceMode.Impulse);
+        }
+    }
+
+    void GroundCheck()
+    {
+        float groundCheckDistance = (GetComponent<CapsuleCollider>().height / 2) + 0.1f;
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, -transform.up, out hit, groundCheckDistance))
+        {
+            _isGrounded = true;
+        }
     }
 }
