58fcb5de172e6183e94b234345a17a2eca2bcc5c
Cade Calcote <cade.m.calcote@gmail.com>
2023-09-07 08:20:52 -0700

    Ramp, ground movement settled

--- Assets/Scripts/PlayerControllerWithCharCont.cs
+++ Assets/Scripts/PlayerControllerWithCharCont.cs
@@ -13,18 +13,10 @@ public class PlayerControllerWithCharCont : MonoBehaviour
     float _fallingVelocity = 0;
     float _speedSmoothTime = 0.01f;    
 
-    [SerializeField][Range(0, 1)] float _airWalk;
-
     [SerializeField] float _rateOfAcceleration;
     [SerializeField] float _maximumSpeed;
     [SerializeField] float _jumpHeight;
     [SerializeField] float _fallRate;
-
-    //Look at how Jump is achieved here
-    //https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
-
-    //events for movement?
-    //https://onewheelstudio.com/blog/2022/4/15/input-event-handlers-or-adding-juice-the-easy-way
 
     private void Awake()
     {
@@ -46,18 +38,5 @@ public class PlayerControllerWithCharCont : MonoBehaviour
         Vector3 movementVector = new Vector3(inputVector.x, -_fallRate, inputVector.y) * _maximumSpeed * Time.deltaTime;
        
         _characterController.Move(movementVector);
-    }
-
-    float GetModifiedSmoothTime(float smoothTime)
-    {
-        if (_characterController.isGrounded)
-        {
-            return smoothTime;
-        }
-        if (_airWalk == 0)
-        {
-            return float.MaxValue;
-        }
-        return smoothTime / _airWalk;
-    }
+    }    
 }
