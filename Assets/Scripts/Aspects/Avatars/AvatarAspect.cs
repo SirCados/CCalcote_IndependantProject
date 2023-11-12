5abeaddd9200d38ac63b1dea11426bf23ce20350
Cade Calcote <cade.m.calcote@gmail.com>
2023-11-11 16:49:26 -0800

    Tenacious Avatar Archetype
    The first real archetype to be introduced. The Tenacious Avatars have a small health regeneration as they regain stability, this can be interrupted if the avatar is knocked down. They also gain Sturdy on their final dash, making them immune to stability loss for .5 seconds.

--- Assets/Scripts/Aspects/Avatars/AvatarAspect.cs
+++ Assets/Scripts/Aspects/Avatars/AvatarAspect.cs
@@ -11,15 +11,19 @@ public class AvatarAspect : MonoBehaviour
     public bool IsInHitStun = false;
     public bool IsInvulnerable = false;
     public bool IsSturdy = false;
+
+    [Header("PUBLIC AVATAR STATS")]
+    public int CurrentHealth;
+    public int CurrentStability;
     public int RemainingAirDashes;
 
     [Header("AVATAR STATS")]
     [Tooltip("How much damage the avatar can take before game over.")]
-    [SerializeField] int _maximumHealth = 3;
+    [SerializeField]protected int _maximumHealth = 3;
     [Tooltip("Reduced damage taken and stabilty loss from hits. Cannot reduce them to 0")]
     [SerializeField][Range(0, 20)] int _defense;
     [Tooltip("If avatar reaches 0 Stabilty, it is knocked down.")]
-    [SerializeField][Range(30, 60)] int _maximumStability;
+    [SerializeField][Range(30, 60)]protected int _maximumStability;
     [Tooltip("How fast an avatar can move")]
     [SerializeField] float _movementSpeed;
     [Tooltip("How fast an avatar can get to top speed.")]
@@ -42,8 +46,6 @@ public class AvatarAspect : MonoBehaviour
     
     Animator _animator;
     IKControl _ikControl;
-    int _currentHealth;
-    public int _currentStability;
     Rigidbody _playerRigidBody;
     Transform _currentTarget;
     Vector3 _dashStartPosition;
@@ -54,14 +56,10 @@ public class AvatarAspect : MonoBehaviour
         SetupAvatarAspect();        
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         HandleJumpAndFallingAnimations();
         RotateCharacter();
-    }
-
-    private void FixedUpdate()
-    {        
         CheckIfDashIsDone();
         PerformHandRaise();
     }
@@ -99,10 +97,11 @@ public class AvatarAspect : MonoBehaviour
     public void PerformAirDash(Vector2 inputVector) 
     {
         IsDashing = true;
+        RemainingAirDashes -= 1;
         _playerRigidBody.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
         _playerRigidBody.useGravity = false;
         _dashVector = ((inputVector != Vector2.zero) ? new Vector3(inputVector.x, 0, inputVector.y) : _avatarModelTransform.forward) * _dashDistance;                       
-        RemainingAirDashes -= 1;
+        
         _dashStartPosition = _playerRigidBody.position;
         _playerRigidBody.AddForce(_dashVector * _dashSpeed, ForceMode.VelocityChange);
     }
@@ -121,37 +120,45 @@ public class AvatarAspect : MonoBehaviour
         }
     }
 
-    public IEnumerator TakeDamage(int incomingDamage)
+    public void TakeHit(int incomingDamage, int incomingStabilityLoss)
+    {
+        StopCoroutine(HandleHit(incomingDamage, incomingStabilityLoss));
+        StartCoroutine(HandleHit(incomingDamage, incomingStabilityLoss));
+    }
+
+    IEnumerator HandleHit(int incomingDamage, int incomingStabilityLoss)
     {
         if (!IsInvulnerable)
         {
+            StopCoroutine(RegainStability());
             int damageToTake = incomingDamage - _defense;
+            CurrentHealth -= (damageToTake > 1) ? damageToTake : 1;
             IsInHitStun = true;
             _animator.SetBool("IsInHitStun", true);
+            if (!IsSturdy)
+            {
+                int stabilityToLose = (incomingStabilityLoss - _defense);
+                CurrentStability -= (stabilityToLose > 1) ? stabilityToLose : 1;
+                if (CurrentStability <= 0)
+                {
+                    CurrentStability = 0;
+                    IsKnockedDown = true;
+                    _animator.SetBool("IsKnockedDown", true);
+                }
+            }
             IsDashing = false;
             yield return new WaitForSecondsRealtime(.1f);
             IsInHitStun = false;
             _animator.SetBool("IsInHitStun", false);
-        }
-        
+            StartCoroutine(RegainStability());
+        }        
     }
     public void LoseStability(int stabiltyLoss)
     {
-        if (!IsSturdy)
-        {
-            int stabilityToLose = (stabiltyLoss - _defense / 2);
-            _currentStability -= (stabilityToLose > 1) ? stabilityToLose : 1;
-            if (_currentStability <= 0)
-            {
-                _currentStability = 0;
-                IsKnockedDown = true;
-                _animator.SetBool("IsKnockedDown", true);
-            }
-            StartCoroutine(RegainStability());
-        }
+        
     }
 
-    public IEnumerator RegainStability()
+    public virtual IEnumerator RegainStability()
     {
         print("started");
         if (IsKnockedDown)
@@ -160,19 +167,19 @@ public class AvatarAspect : MonoBehaviour
             GetUpSequence();            
         }
         yield return new WaitForSecondsRealtime(3);
-        while (_currentStability < _maximumStability && !IsKnockedDown)
+        while (CurrentStability < _maximumStability && !IsKnockedDown)
         {
-            _currentStability += 1;
+            CurrentStability += 1;
             yield return new WaitForSecondsRealtime(1f);
         }
     }
 
-    void GetUpSequence()
+    protected void GetUpSequence()
     {
         IsKnockedDown = false;
         _animator.SetBool("IsKnockedDown", false);
         StartCoroutine(Invulerability());
-        _currentStability = _maximumStability;
+        CurrentStability = _maximumStability;
     }
 
     IEnumerator Invulerability()
@@ -275,8 +282,8 @@ public class AvatarAspect : MonoBehaviour
 
     void SetupAvatarAspect()
     {
-        _currentHealth = _maximumHealth;
-        _currentStability = _maximumStability;
+        CurrentHealth = _maximumHealth;
+        CurrentStability = _maximumStability;
         ResetAirDashes();
         _animator = GetComponentInChildren<Animator>();
         _ikControl = GetComponentInChildren<IKControl>();
--- Assets/Scripts/Aspects/Avatars/SwiftAvatar.cs
+++ Assets/Scripts/Aspects/Avatars/SwiftAvatar.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BezierCurve : MonoBehaviour
+public class SwiftAvatar : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
